8c8da7ac9839fa6fa82e46270a53ca0406aaafe1
Md Taufiquel Islam <taufiqalahi940@gmail.com>
2025-03-20 20:47:21 +0600

    main

---
+++ app/src/main/java/com/techmnia/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.techmnia.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            enableEdgeToEdge()
+            setContentView(R.layout.activity_main)
+
+
+        }
+    }
+}
